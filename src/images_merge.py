9c3bfa809d91fdc81a46e4d0304924ca83005664
pzia <paparazzia@gmail.com>
2015-05-23 18:18:42 +0200

    --quick flag to speed up folder merging

--- src/images_merge.py
+++ src/images_merge.py
@@ -10,10 +10,11 @@ class KmdImagesMerge(KmdCmd.KmdCommand):
         super(KmdImagesMerge, self).extendParser()
         #Extend parser
         self.parser.add_argument('folders', metavar='</path/to/folder>', nargs='+', help='A list of folders')
+        self.parser.add_argument('--quick', action="store_true", help='Compare only if size differs for more than 1000 bytes')
 
     def run(self):
         for folder in cmd.args.folders :
-            KmdImages.cleanDir(folder, cmd.args.doit)
+            KmdImages.cleanDir(folder, cmd.args.doit, quick = cmd.args.quick)
 
 if __name__ == "__main__":
     cmd = KmdImagesMerge(__doc__)
